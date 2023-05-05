ec5473d2c76a6eb497a9fa17bc90f04dee1074f5
Matteo Macrì <matteomacri18@gmail.com>
2023-05-05 15:46:04 +0200

    Show cookie banner

--- src/App.js
+++ src/App.js
@@ -7,25 +7,40 @@ import Projects from "./pages/Projects";
 import Courses from "./pages/Courses";
 import Contacts from "./pages/Contacts";
 import ReactGA from 'react-ga';
+import CookieBanner from "./components/CookieBanner";
+import Policy from "./pages/Policy";
 
 const TRAKING_ID = "G-X2SJDWTRTV";
 ReactGA.initialize(TRAKING_ID);
 
 function App() {
+  const [showBanner, setShowBanner] = React.useState(false);
 
   React.useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
+
+    const hasConsent = localStorage.getItem('cookieConsent');
+    setShowBanner(!hasConsent);
   }, []);
 
+  const handleConsent = () => {
+    localStorage.setItem('cookieConsent', true);
+    setShowBanner(false);
+  };
+
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/about" element={<AboutMe />} />
-      <Route exact path="/posts" element={<Posts />} />
-      <Route exact path="/projects" element={<Projects />} />
-      <Route exact path="/courses" element={<Courses />} />
-      <Route exact path="/contacts" element={<Contacts />} />
-    </Routes>
+    <>
+      {showBanner && <CookieBanner onConsent={handleConsent} />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<AboutMe />} />
+        <Route exact path="/posts" element={<Posts />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/courses" element={<Courses />} />
+        <Route exact path="/contacts" element={<Contacts />} />
+        <Route exact path="/policy" element={<Policy />} />
+      </Routes>
+    </>
   );
 }
 
