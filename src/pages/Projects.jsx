38cc726ca17a90dcfecee679c16bdae3a5f374b9
Matteo Macrì <matteomacri18@gmail.com>
2023-05-20 20:28:28 +0200

    Add title

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,8 +15,12 @@ const Projects = () => {
   return (
     <div>
       <Navbar />
-      <div className='bg-dark lg:py-24 md:pb-20 2xl:py-36'>
-        
+      <div className='bg-dark lg:py-24 md:pb-20 2xl:py-36' style={{ marginBottom: 250 }}>
+        <div className='flex justify-center sm:pt-16'>
+          <p className='text-2xl text-gray-100 font-syne tracking-widest leading-1 font-bold mx-10'>
+            COMING SOON ...
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
